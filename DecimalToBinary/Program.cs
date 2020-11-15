3b0fd25e84b457d4f5bcf410a014283ea8caaeb5
Giorgos Gial <g.gialouris@outlook.com.gr>
2020-11-15 19:37:32 +0200

    Add project files.

---
+++ DecimalToBinary/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numb;
+            Console.Write("Enter an integer number : ");
+            numb = int.Parse(Console.ReadLine());
+            int quot;
+            string rem = "";
+            while (numb >= 1)
+            {
+                quot = numb / 2;
+                rem += (numb % 2).ToString();
+                numb = quot;
+            }
+            string bin = "";
+            for (int i = rem.Length - 1; i >= 0; i--)
+            {
+                bin = bin + rem[i];
+            }
+            Console.WriteLine("The binary format for number {0} is {1}", numb, bin);
+            Console.Read();
+        }
+    }
+}
